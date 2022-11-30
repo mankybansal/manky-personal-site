c93e8170496112686efc4564258dea79e5af85a8
Mayank Bansal <sunny.bansal@gmail.com>
2022-11-30 13:16:17 -0800

    Add meet link

---
+++ pages/meet.js
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+
+export default function Resume() {
+  useEffect(() => {
+    window.location = "https://calend.ly/mankybansal";
+  }, []);
+
+  return <></>;
+}
